053a5b7cf03496027fff12629b925fa71a962c82
phiac <45037147+phiac@users.noreply.github.com>
2025-03-22 17:41:48 -0100

    Update settings.py

--- myvoice/settings.py
+++ myvoice/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME', 'railway'),
         'USER': os.environ.get('DATABASE_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'WaWJKWDltkMPftyvbYEfzchrRuUPGoJj'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'cSbfGmwcpgeyaOqFWwpbTBuDFHiuJcjC'),
         'HOST': os.environ.get('DATABASE_HOST', 'postgres.railway.internal'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
     }
