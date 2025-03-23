189f9f0fd6bc8ee0aa76cb13ba243dfd9881fae1
phiac <45037147+phiac@users.noreply.github.com>
2025-03-23 17:55:22 -0100

    Update settings.py

--- myvoice/settings.py
+++ myvoice/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "django-insecure-y0&g8dvlox@d92kfqfl+y%ad2ct)go+*+$)a7h7c+gsqpq@^bf
 if os.environ.get('DEBUG', 'False') == 'True':  # For local development
     ALLOWED_HOSTS = 'localhost', '127.0.0.1'
 else:  # For production
-    ALLOWED_HOSTS = 'postgres-production-a225.up.railway.app'
+    ALLOWED_HOSTS = '*'
 
 
 
